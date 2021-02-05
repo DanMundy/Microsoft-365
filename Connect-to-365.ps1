17b3fce7b35c1e7046c50c568cb8c4b436f6da6b
Dan <dan@mundy.co>
2021-02-05 16:46:43 +1030

    Update Connect-to-365.ps1

--- Connect-to-365.ps1
+++ Connect-to-365.ps1
@@ -1,5 +1,6 @@
 # Note the following are not supported in PSv7,
 # In VS Code, terminal drop-down = "powershell" (v5)
 # NOT "PowerShell Integrated Console" (v7)
+
 Import-Module MSOnline
 Connect-MsolService
