cdedcfbc218f1168e2f3c058ab1cb2fecbc1115d
Dan <dan@mundy.co>
2021-04-08 13:18:31 +0930

    Update Email-Addresses.ps1

--- Email-Addresses.ps1
+++ Email-Addresses.ps1
@@ -1,3 +1,3 @@
 Import-Module MSOnline
 Connect-MsolService
-Get-Recipient -Filter { EmailAddress –like 'smtp:ims*' –or EmailAddress –like 'ims*' } -Properties Name,Emailaddresses | select Name,Emailaddresses -ExpandProperty Emailaddresses
+Get-Recipient -Filter { EmailAddress –like 'smtp:ims*' –or EmailAddress –like 'ims*' } -Properties Name,Emailaddresses | Select-Object Name,Emailaddresses -ExpandProperty Emailaddresses
